8fa99d0688098f29c9d6ab0de1e51b9d8b57b2c8
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2023-05-13 22:36:42 +0200

    WIP: Fix mesh-1.1

--- tests/bsim/bluetooth/mesh/tests_scripts/friendship/msg_va_collision.sh
+++ tests/bsim/bluetooth/mesh/tests_scripts/friendship/msg_va_collision.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2021 Nordic Semiconductor
+# Copyright 2023 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
 source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
@@ -9,8 +9,7 @@ RunTest mesh_friendship_msg_va_collision \
 	friendship_lpn_va_collision \
 	friendship_friend_va_collision
 
-#conf=prj_mesh1d1_conf
-#RunTest mesh_friendship_msg_group_1d1 \
-#	friendship_lpn_group \
-#	friendship_other_group \
-#	friendship_friend_group
+conf=prj_mesh1d1_conf
+RunTest mesh_friendship_msg_group_1d1 \
+	friendship_lpn_va_collision \
+	friendship_friend_va_collision
--- tests/bsim/bluetooth/mesh/tests_scripts/transport/va_collision.sh
+++ tests/bsim/bluetooth/mesh/tests_scripts/transport/va_collision.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2021 Nordic Semiconductor
+# Copyright 2023 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
 source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
