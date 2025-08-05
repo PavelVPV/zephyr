f6dd0fdc98ce56387cb48efa305e0bdfe456eb00
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2025-08-05 15:43:17 +0200

    Set BT_CONN_MAX to 231
    It is max that controller supports on multicore.
    Also increase BT_MAX_PAIRED to align with BT_MAX_CONN

--- tests/bsim/bluetooth/ll/multiple_id/tests_scripts/multiple.sh
+++ tests/bsim/bluetooth/ll/multiple_id/tests_scripts/multiple.sh
@@ -11,11 +11,11 @@ EXECUTE_TIMEOUT=1600
 
 cd ${BSIM_OUT_PATH}/bin
 
-#Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_multiple_id_prj_conf -RealEncryption=1 \
-#  -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=central
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_multiple_id_prj_conf -RealEncryption=1 \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=central
 
-#Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_multiple_id_prj_conf -RealEncryption=1 \
-#  -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=peripheral -rs=6
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_multiple_id_prj_conf -RealEncryption=1 \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=peripheral -rs=6
 
 Execute ./bs_2G4_phy_v1 -dump -v=${verbosity_level} -s=${simulation_id} \
   -D=2 -sim_length=1800e6 $@ -argschannel -at=40
